1fe78f064d7fd0468e7618aa4075b227ee861058
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-01-03 23:08:10 +0900

    標準入力２

--- index.go
+++ index.go
@@ -4,21 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
-// 文字列を1行入力
-func StrStdin() (stringInput string) {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	scanner.Scan()
-	stringInput = scanner.Text()
-
-	stringInput = strings.TrimSpace(stringInput)
-	return
-}
-
 func main() {
-	p := StrStdin()
-	fmt.Println("結果 "+ p)
+	sc := bufio.NewScanner(os.Stdin)
+
+	sc.Scan()
+	name := sc.Text()
+	fmt.Println("Hello" + name)
 }
