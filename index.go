ac4ba785f01c4d3eab7945e37beaf306a9f5828e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-01-14 20:15:52 +0900

    複数行を受け取る

--- index.go
+++ index.go
@@ -11,20 +11,14 @@ func main() {
 
 	sc := bufio.NewScanner(os.Stdin)
 
-	// 入力タブから文字列データを受け取る
-	sc.Scan()
-	name := sc.Text()
-	fmt.Println("Hello" + name)
-
-	// 入力タブから数字を受け取る
 	sc.Scan()
 	count, _ := strconv.Atoi(sc.Text())
 	fmt.Println(count)
 
-	greeting := "Hello World"
-
 	for i := 0; i < count; i++ {
-		fmt.Println(greeting)
+		sc.Scan()
+		name := sc.Text()
+		fmt.Println("Hello " + name)
 	}
 
 }
