3ae45d82452f5fb45235f195a257c2684a2ad3be
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-01-14 19:11:29 +0900

    入力タブから数字を受け取る

--- index.go
+++ index.go
@@ -4,13 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	// 入力タブから文字列データを受け取る
+	
 	sc := bufio.NewScanner(os.Stdin)
 
+	// 入力タブから文字列データを受け取る
 	sc.Scan()
 	name := sc.Text()
 	fmt.Println("Hello" + name)
+
+	// 入力タブから数字を受け取る
+	sc.Scan()
+	number, _ := strconv.Atoi(sc.Text())
+	fmt.Println(number)
 }
